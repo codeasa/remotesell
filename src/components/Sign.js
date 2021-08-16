f39cced4f6e50f5bd9094057f6dbf78187e80e46
codeasa <FWDASC@hk.fwd.com>
2021-08-16 20:10:25 +0800

    X

--- src/components/Sign.js
+++ src/components/Sign.js
@@ -1,7 +1,15 @@
+import SignatureCanvas from "react-signature-canvas";
+
 export const Sign = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <div>
+      <h2 className="p-4">Signature</h2>
+      <div className="bg-gray-50">
+        <SignatureCanvas
+          penColor="green"
+          canvasProps={{ width: 500, height: 200, className: "sigCanvas" }}
+        />
+      </div>
+    </div>
+  );
+};
