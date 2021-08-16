37d5b7f02554f31d8b5f118ba390ba6416e06242
codeasa <FWDASC@hk.fwd.com>
2021-08-16 23:10:44 +0800

    X

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,9 +4,8 @@ import Button from "@material-ui/core/Button";
 import styled from "styled-components";
 
 export const Login = () => {
-  const cButton = styled(Button)`
-    color: red;
-  `;
+
+ 
   return (
     <div className="w-full    h-full p-10 bg-gray-50">
       <div className="place-content-center flex-col mb-4">
